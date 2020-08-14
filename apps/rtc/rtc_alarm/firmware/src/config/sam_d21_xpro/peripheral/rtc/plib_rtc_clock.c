b91bf3d25dd42affe2a83765bdd743c476178020
Arpan Kumar <arpan.kumar@microchip.com>
2020-08-14 22:26:39 +0530

    [RTC] Regenerated demo for rtc_rtc_alarm_sam_d21_xpro
    MH3-39998
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>
    
    [RTC] Regenerated demo for rtc_rtc_alarm_sam_d21_xpro
    
    MH3-39998
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/rtc/rtc_alarm/firmware/src/config/sam_d21_xpro/peripheral/rtc/plib_rtc_clock.c
+++ apps/rtc/rtc_alarm/firmware/src/config/sam_d21_xpro/peripheral/rtc/plib_rtc_clock.c
@@ -70,45 +70,46 @@
 
 RTC_OBJECT rtcObj;
 
+static void RTC_ClockReadSynchronization(void)
+{
+   /* Read-synchronization for CLOCK register */
+   RTC_REGS->MODE2.RTC_READREQ = RTC_READREQ_RREQ_Msk | RTC_READREQ_ADDR(0x10);
+   while((RTC_REGS->MODE2.RTC_STATUS & RTC_STATUS_SYNCBUSY_Msk) == RTC_STATUS_SYNCBUSY_Msk)
+   {
+       /* Wait for Read-Synchronization */
+   }
+}
+
 void RTC_Initialize(void)
 {
+    /* Writing to CTRL register will trigger write-synchronization */
     RTC_REGS->MODE2.RTC_CTRL |= RTC_MODE2_CTRL_SWRST_Msk;
-
-    RTC_REGS->MODE2.RTC_READREQ |= RTC_READREQ_RCONT_Msk;
-
     while((RTC_REGS->MODE2.RTC_STATUS & RTC_STATUS_SYNCBUSY_Msk) == RTC_STATUS_SYNCBUSY_Msk)
     {
-        /* Wait for synchronization after Software Reset */
+        /* Wait for Write-Synchronization */
     }
 
+    /* Writing to CTRL register will trigger write-synchronization */
     RTC_REGS->MODE2.RTC_CTRL = RTC_MODE2_CTRL_MODE(2) | RTC_MODE2_CTRL_PRESCALER(0xA) | RTC_MODE2_CTRL_ENABLE_Msk;
-
-
     while((RTC_REGS->MODE2.RTC_STATUS & RTC_STATUS_SYNCBUSY_Msk) == RTC_STATUS_SYNCBUSY_Msk)
     {
-        /* Wait for Synchronization after Enabling RTC */
+        /* Wait for Write-Synchronization */
     }
-
 
 }
 
 bool RTC_RTCCTimeSet (struct tm * initialTime )
 {
-    /*
-     * Add 1900 to the tm_year member and the adjust for the RTC reference year
-     * Set YEAR(according to Reference Year), MONTH and DAY
-     *set Hour Minute and Second
-     */
+    /* Writing to CLOCK register will trigger write-synchronization */
     RTC_REGS->MODE2.RTC_CLOCK = ((TM_STRUCT_REFERENCE_YEAR + initialTime->tm_year) - REFERENCE_YEAR) << RTC_MODE2_CLOCK_YEAR_Pos |
                     ((ADJUST_MONTH(initialTime->tm_mon)) << RTC_MODE2_CLOCK_MONTH_Pos) |
                     (initialTime->tm_mday << RTC_MODE2_CLOCK_DAY_Pos) |
                     (initialTime->tm_hour << RTC_MODE2_CLOCK_HOUR_Pos) |
                     (initialTime->tm_min << RTC_MODE2_CLOCK_MINUTE_Pos) |
                     (initialTime->tm_sec << RTC_MODE2_CLOCK_SECOND_Pos);
-
     while((RTC_REGS->MODE2.RTC_STATUS & RTC_STATUS_SYNCBUSY_Msk) == RTC_STATUS_SYNCBUSY_Msk)
     {
-        /* Synchronization after writing value to CLOCK Register */
+        /* Wait for Write-Synchronization */
     }
     return true;
 }
@@ -117,11 +118,8 @@ void RTC_RTCCTimeGet ( struct tm * currentTime )
 {
     uint32_t dataClockCalendar = 0;
 
-    while((RTC_REGS->MODE2.RTC_STATUS & RTC_STATUS_SYNCBUSY_Msk) == RTC_STATUS_SYNCBUSY_Msk)
-    {
-        /* Synchronization before reading value from CLOCK Register */
-    }
-
+    /* Enable read-synchronization for CLOCK register to avoid CPU stall */
+    RTC_ClockReadSynchronization();
     dataClockCalendar = RTC_REGS->MODE2.RTC_CLOCK;
 
     currentTime->tm_hour =  (dataClockCalendar & RTC_MODE2_CLOCK_HOUR_Msk) >> RTC_MODE2_CLOCK_HOUR_Pos;
@@ -135,51 +133,45 @@ void RTC_RTCCTimeGet ( struct tm * currentTime )
 
 bool RTC_RTCCAlarmSet (struct tm * alarmTime, RTC_ALARM_MASK mask)
 {
-    /*
-     * Add 1900 to the tm_year member and the adjust for the RTC reference year
-     * Set YEAR(according to Reference Year), MONTH and DAY
-     * Set Hour, Minute and second
-     */
-    RTC_REGS->MODE2.RTC_ALARM = ((TM_STRUCT_REFERENCE_YEAR + alarmTime->tm_year) - REFERENCE_YEAR) << RTC_MODE2_CLOCK_YEAR_Pos |
+   /* Writing to ALARM register will trigger write-synchronization */
+   RTC_REGS->MODE2.RTC_ALARM = ((TM_STRUCT_REFERENCE_YEAR + alarmTime->tm_year) - REFERENCE_YEAR) << RTC_MODE2_CLOCK_YEAR_Pos |
                     (ADJUST_MONTH(alarmTime->tm_mon) << RTC_MODE2_CLOCK_MONTH_Pos) |
                     (alarmTime->tm_mday << RTC_MODE2_CLOCK_DAY_Pos) |
                     (alarmTime->tm_hour << RTC_MODE2_CLOCK_HOUR_Pos) |
                      (alarmTime->tm_min << RTC_MODE2_CLOCK_MINUTE_Pos) |
                      (alarmTime->tm_sec << RTC_MODE2_CLOCK_SECOND_Pos);
+   while((RTC_REGS->MODE2.RTC_STATUS & RTC_STATUS_SYNCBUSY_Msk) == RTC_STATUS_SYNCBUSY_Msk)
+   {
+       /* Wait for Write-Synchronization */
+   }
 
-    while((RTC_REGS->MODE2.RTC_STATUS & RTC_STATUS_SYNCBUSY_Msk) == RTC_STATUS_SYNCBUSY_Msk)
-    {
-        /* Synchronization after writing to ALARM register */
-    }
+   /* Writing to MASK register will trigger write-synchronization */
+   RTC_REGS->MODE2.RTC_MASK = mask;
+   while((RTC_REGS->MODE2.RTC_STATUS & RTC_STATUS_SYNCBUSY_Msk) == RTC_STATUS_SYNCBUSY_Msk)
+   {
+       /* Wait for Write-Synchronization */
+   }
 
-    RTC_REGS->MODE2.RTC_MASK = mask;
-
-    while((RTC_REGS->MODE2.RTC_STATUS & RTC_STATUS_SYNCBUSY_Msk) == RTC_STATUS_SYNCBUSY_Msk)
-    {
-        /* Synchronization after writing value to MASK Register */
-    }
-
-    RTC_REGS->MODE2.RTC_INTENSET = RTC_MODE2_INTENSET_ALARM0_Msk;
-
-    return true;
+   RTC_REGS->MODE2.RTC_INTENSET = RTC_MODE2_INTENSET_ALARM0_Msk;
+   return true;
 }
 
 void RTC_RTCCCallbackRegister ( RTC_CALLBACK callback, uintptr_t context)
 {
-    rtcObj.alarmCallback = callback;
-    rtcObj.context       = context;
+   rtcObj.alarmCallback = callback;
+   rtcObj.context       = context;
 }
 
 
 void RTC_InterruptHandler(void)
 {
-    rtcObj.intCause = (RTC_CLOCK_INT_MASK) RTC_REGS->MODE2.RTC_INTFLAG;
+   rtcObj.intCause = (RTC_CLOCK_INT_MASK) RTC_REGS->MODE2.RTC_INTFLAG;
 
-    /* Clear All Interrupts */
-    RTC_REGS->MODE2.RTC_INTFLAG = RTC_MODE2_INTFLAG_Msk;
-    
-    if(rtcObj.alarmCallback != NULL)
-    {
-        rtcObj.alarmCallback(rtcObj.intCause, rtcObj.context);
-    }
+   /* Clear All Interrupts */
+   RTC_REGS->MODE2.RTC_INTFLAG = RTC_MODE2_INTFLAG_Msk;
+
+   if(rtcObj.alarmCallback != NULL)
+   {
+       rtcObj.alarmCallback(rtcObj.intCause, rtcObj.context);
+   }
 }
