e233be8ed74bccb039129f3b3b60c9bf7731a85f
Arpan Kumar <arpan.kumar@microchip.com>
2020-08-14 22:35:57 +0530

    [CLOCK] Regenerated demo for clock_clock_config_sam_d21_xpro
    MH3-39950
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>
    
    [CLOCK] Regenerated demo for clock_clock_config_sam_d21_xpro
    
    MH3-39950
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/clock/clock_config/firmware/src/config/sam_d21_xpro/peripheral/clock/plib_clock.c
+++ apps/clock/clock_config/firmware/src/config/sam_d21_xpro/peripheral/clock/plib_clock.c
@@ -47,7 +47,6 @@ static void SYSCTRL_Initialize(void)
 
     /* Configure 32K External Oscillator */
     SYSCTRL_REGS->SYSCTRL_XOSC32K = SYSCTRL_XOSC32K_STARTUP(0) | SYSCTRL_XOSC32K_ENABLE_Msk | SYSCTRL_XOSC32K_EN32K_Msk | SYSCTRL_XOSC32K_XTALEN_Msk;
-
     while(!((SYSCTRL_REGS->SYSCTRL_PCLKSR & SYSCTRL_PCLKSR_XOSC32KRDY_Msk) == SYSCTRL_PCLKSR_XOSC32KRDY_Msk))
     {
         /* Waiting for the XOSC32K Ready state */
@@ -87,7 +86,7 @@ static void DFLL_Initialize(void)
 
 static void GCLK0_Initialize(void)
 {
-    
+
     GCLK_REGS->GCLK_GENCTRL = GCLK_GENCTRL_SRC(7) | GCLK_GENCTRL_RUNSTDBY_Msk | GCLK_GENCTRL_OE_Msk | GCLK_GENCTRL_GENEN_Msk | GCLK_GENCTRL_ID(0);
 
     while((GCLK_REGS->GCLK_STATUS & GCLK_STATUS_SYNCBUSY_Msk) == GCLK_STATUS_SYNCBUSY_Msk)
@@ -118,7 +117,7 @@ void CLOCK_Initialize (void)
     GCLK1_Initialize();
 
 
-    
+
 
     /*Disable RC oscillator*/
     SYSCTRL_REGS->SYSCTRL_OSC8M = 0x0;
