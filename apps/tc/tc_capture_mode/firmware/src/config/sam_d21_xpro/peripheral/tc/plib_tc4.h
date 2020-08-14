73035b3637c17f71ccd1025f10c80eeb88083ac0
Arpan Kumar <arpan.kumar@microchip.com>
2020-08-14 22:25:08 +0530

    [TC] Regenerated demo for tc_tc_capture_mode_sam_d21_xpro
    MH3-40050
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>
    
    [TC] Regenerated demo for tc_tc_capture_mode_sam_d21_xpro
    
    MH3-40050
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/tc/tc_capture_mode/firmware/src/config/sam_d21_xpro/peripheral/tc/plib_tc4.h
+++ apps/tc/tc_capture_mode/firmware/src/config/sam_d21_xpro/peripheral/tc/plib_tc4.h
@@ -102,6 +102,8 @@ uint16_t TC4_Capture16bitChannel1Get( void );
 void TC4_CaptureCallbackRegister(TC_CAPTURE_CALLBACK callback, uintptr_t context);
 
 
+void TC4_CaptureCommandSet(TC_COMMAND command);
+
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
