bb79e1e9ede6b082d1c4fed1d715bd5457fe1cf7
Arpan Kumar <arpan.kumar@microchip.com>
2020-08-14 22:20:53 +0530

    [RTC] Regenerated demo for rtc_rtc_periodic_timeout_sam_d21_xpro
    MH3-40002
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>
    
    [RTC] Regenerated demo for rtc_rtc_periodic_timeout_sam_d21_xpro
    
    MH3-40002
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/rtc/rtc_periodic_timeout/firmware/src/config/sam_d21_xpro/peripheral/clock/plib_clock.c
+++ apps/rtc/rtc_periodic_timeout/firmware/src/config/sam_d21_xpro/peripheral/clock/plib_clock.c
@@ -47,7 +47,6 @@ static void SYSCTRL_Initialize(void)
 
     /* Configure 32K External Oscillator */
     SYSCTRL_REGS->SYSCTRL_XOSC32K = SYSCTRL_XOSC32K_STARTUP(0) | SYSCTRL_XOSC32K_ENABLE_Msk | SYSCTRL_XOSC32K_EN32K_Msk | SYSCTRL_XOSC32K_XTALEN_Msk;
-
     while(!((SYSCTRL_REGS->SYSCTRL_PCLKSR & SYSCTRL_PCLKSR_XOSC32KRDY_Msk) == SYSCTRL_PCLKSR_XOSC32KRDY_Msk))
     {
         /* Waiting for the XOSC32K Ready state */
@@ -57,7 +56,6 @@ static void SYSCTRL_Initialize(void)
     uint32_t calibValue = (uint32_t)(((*(uint64_t*)0x806020) >> 38 ) & 0x7f);
     /* Configure 32K RC oscillator */
     SYSCTRL_REGS->SYSCTRL_OSC32K = SYSCTRL_OSC32K_CALIB(calibValue) | SYSCTRL_OSC32K_STARTUP(0) | SYSCTRL_OSC32K_ENABLE_Msk | SYSCTRL_OSC32K_EN32K_Msk ;
-
     while(!((SYSCTRL_REGS->SYSCTRL_PCLKSR & SYSCTRL_PCLKSR_OSC32KRDY_Msk) == SYSCTRL_PCLKSR_OSC32KRDY_Msk))
     {
         /* Waiting for the OSC32K Ready state */
@@ -98,7 +96,7 @@ static void DFLL_Initialize(void)
 
 static void GCLK0_Initialize(void)
 {
-    
+
     GCLK_REGS->GCLK_GENCTRL = GCLK_GENCTRL_SRC(7) | GCLK_GENCTRL_GENEN_Msk | GCLK_GENCTRL_ID(0);
 
     while((GCLK_REGS->GCLK_STATUS & GCLK_STATUS_SYNCBUSY_Msk) == GCLK_STATUS_SYNCBUSY_Msk)
@@ -143,7 +141,7 @@ void CLOCK_Initialize (void)
 
     /* Selection of the Generator and write Lock for RTC */
     GCLK_REGS->GCLK_CLKCTRL = GCLK_CLKCTRL_ID(4) | GCLK_CLKCTRL_GEN(0x1)  | GCLK_CLKCTRL_CLKEN_Msk;
-    
+
 
     /*Disable RC oscillator*/
     SYSCTRL_REGS->SYSCTRL_OSC8M = 0x0;
