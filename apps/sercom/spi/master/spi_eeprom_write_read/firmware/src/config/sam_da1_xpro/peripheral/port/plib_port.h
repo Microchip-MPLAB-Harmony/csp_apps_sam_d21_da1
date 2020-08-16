e5cee0c531da0b3068fe9432ea7b0739834fb3fa
Arpan Kumar <arpan.kumar@microchip.com>
2020-08-16 23:08:12 +0530

    [SERCOM] Regenerated demo for sercom_spi_master_spi_eeprom_write_read_sam_da1_xpro
    MH3-40016
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/sercom/spi/master/spi_eeprom_write_read/firmware/src/config/sam_da1_xpro/peripheral/port/plib_port.h
+++ apps/sercom/spi/master/spi_eeprom_write_read/firmware/src/config/sam_da1_xpro/peripheral/port/plib_port.h
@@ -65,45 +65,41 @@
 // *****************************************************************************
 // *****************************************************************************
 
-  
 /*** Macros for EEPROM_HOLD pin ***/
 #define EEPROM_HOLD_Set()               (PORT_REGS->GROUP[1].PORT_OUTSET = 1 << 12)
 #define EEPROM_HOLD_Clear()             (PORT_REGS->GROUP[1].PORT_OUTCLR = 1 << 12)
 #define EEPROM_HOLD_Toggle()            (PORT_REGS->GROUP[1].PORT_OUTTGL = 1 << 12)
-#define EEPROM_HOLD_Get()               (((PORT_REGS->GROUP[1].PORT_IN >> 12)) & 0x01)
 #define EEPROM_HOLD_OutputEnable()      (PORT_REGS->GROUP[1].PORT_DIRSET = 1 << 12)
 #define EEPROM_HOLD_InputEnable()       (PORT_REGS->GROUP[1].PORT_DIRCLR = 1 << 12)
+#define EEPROM_HOLD_Get()               (((PORT_REGS->GROUP[1].PORT_IN >> 12)) & 0x01)
 #define EEPROM_HOLD_PIN                  PORT_PIN_PB12
 
 /*** Macros for EEPROM_CS pin ***/
 #define EEPROM_CS_Set()               (PORT_REGS->GROUP[0].PORT_OUTSET = 1 << 17)
 #define EEPROM_CS_Clear()             (PORT_REGS->GROUP[0].PORT_OUTCLR = 1 << 17)
 #define EEPROM_CS_Toggle()            (PORT_REGS->GROUP[0].PORT_OUTTGL = 1 << 17)
-#define EEPROM_CS_Get()               (((PORT_REGS->GROUP[0].PORT_IN >> 17)) & 0x01)
 #define EEPROM_CS_OutputEnable()      (PORT_REGS->GROUP[0].PORT_DIRSET = 1 << 17)
 #define EEPROM_CS_InputEnable()       (PORT_REGS->GROUP[0].PORT_DIRCLR = 1 << 17)
+#define EEPROM_CS_Get()               (((PORT_REGS->GROUP[0].PORT_IN >> 17)) & 0x01)
 #define EEPROM_CS_PIN                  PORT_PIN_PA17
 
 /*** Macros for EEPROM_WP pin ***/
 #define EEPROM_WP_Set()               (PORT_REGS->GROUP[0].PORT_OUTSET = 1 << 20)
 #define EEPROM_WP_Clear()             (PORT_REGS->GROUP[0].PORT_OUTCLR = 1 << 20)
 #define EEPROM_WP_Toggle()            (PORT_REGS->GROUP[0].PORT_OUTTGL = 1 << 20)
-#define EEPROM_WP_Get()               (((PORT_REGS->GROUP[0].PORT_IN >> 20)) & 0x01)
 #define EEPROM_WP_OutputEnable()      (PORT_REGS->GROUP[0].PORT_DIRSET = 1 << 20)
 #define EEPROM_WP_InputEnable()       (PORT_REGS->GROUP[0].PORT_DIRCLR = 1 << 20)
+#define EEPROM_WP_Get()               (((PORT_REGS->GROUP[0].PORT_IN >> 20)) & 0x01)
 #define EEPROM_WP_PIN                  PORT_PIN_PA20
 
 /*** Macros for LED pin ***/
 #define LED_Set()               (PORT_REGS->GROUP[1].PORT_OUTSET = 1 << 30)
 #define LED_Clear()             (PORT_REGS->GROUP[1].PORT_OUTCLR = 1 << 30)
 #define LED_Toggle()            (PORT_REGS->GROUP[1].PORT_OUTTGL = 1 << 30)
-#define LED_Get()               (((PORT_REGS->GROUP[1].PORT_IN >> 30)) & 0x01)
 #define LED_OutputEnable()      (PORT_REGS->GROUP[1].PORT_DIRSET = 1 << 30)
 #define LED_InputEnable()       (PORT_REGS->GROUP[1].PORT_DIRCLR = 1 << 30)
+#define LED_Get()               (((PORT_REGS->GROUP[1].PORT_IN >> 30)) & 0x01)
 #define LED_PIN                  PORT_PIN_PB30
-
-
-
 // *****************************************************************************
 /* PORT Group
 
@@ -913,7 +909,7 @@ void PORT_GroupToggle(PORT_GROUP group, uint32_t mask);
     void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask)
 
   Summary:
-    Confgiures the selected IO pins of a group as input.
+    Configures the selected IO pins of a group as input.
 
   Description:
     This function configures the selected IO pins of a group as input. The pins
@@ -952,7 +948,7 @@ void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask);
     void PORT_GroupOutputEnable(PORT_GROUP group, uint32_t mask)
 
   Summary:
-    Confgiures the selected IO pins of a group as output.
+    Configures the selected IO pins of a group as output.
 
   Description:
     This function configures the selected IO pins of a group as output. The pins
