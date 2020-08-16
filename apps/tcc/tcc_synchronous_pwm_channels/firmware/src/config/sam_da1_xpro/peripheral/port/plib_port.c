72267b031a9e2fea0dec375a91e5e9df30bac9d0
Arpan Kumar <arpan.kumar@microchip.com>
2020-08-16 23:31:28 +0530

    [TCC] Regenerated demo for tcc_tcc_synchronous_pwm_channels_sam_da1_xpro
    MH3-40064
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/tcc/tcc_synchronous_pwm_channels/firmware/src/config/sam_da1_xpro/peripheral/port/plib_port.c
+++ apps/tcc/tcc_synchronous_pwm_channels/firmware/src/config/sam_da1_xpro/peripheral/port/plib_port.c
@@ -75,17 +75,16 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PINCFG[8] = 0x1;
    PORT_REGS->GROUP[0].PORT_PINCFG[9] = 0x1;
    PORT_REGS->GROUP[0].PORT_PINCFG[10] = 0x1;
-   PORT_REGS->GROUP[0].PORT_PINCFG[15] = 0x1;
 
    PORT_REGS->GROUP[0].PORT_PMUX[4] = 0x44;
    PORT_REGS->GROUP[0].PORT_PMUX[5] = 0x5;
-   PORT_REGS->GROUP[0].PORT_PMUX[7] = 0x50;
                 
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_PINCFG[10] = 0x1;
+   PORT_REGS->GROUP[1].PORT_PINCFG[11] = 0x1;
    PORT_REGS->GROUP[1].PORT_PINCFG[12] = 0x1;
 
-   PORT_REGS->GROUP[1].PORT_PMUX[5] = 0x5;
+   PORT_REGS->GROUP[1].PORT_PMUX[5] = 0x55;
    PORT_REGS->GROUP[1].PORT_PMUX[6] = 0x5;
                 
 }
@@ -243,7 +242,7 @@ void PORT_GroupToggle(PORT_GROUP group, uint32_t mask)
     void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask)
 
   Summary:
-    Confgiures the selected IO pins of a group as input.
+    Configures the selected IO pins of a group as input.
 
   Description:
     This function configures the selected IO pins of a group as input. The pins
@@ -264,7 +263,7 @@ void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask)
     void PORT_GroupOutputEnable(PORT_GROUP group, uint32_t mask)
 
   Summary:
-    Confgiures the selected IO pins of a group as output.
+    Configures the selected IO pins of a group as output.
 
   Description:
     This function configures the selected IO pins of a group as output. The pins
