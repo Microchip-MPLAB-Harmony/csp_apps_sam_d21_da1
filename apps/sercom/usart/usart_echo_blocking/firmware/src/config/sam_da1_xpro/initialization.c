db46ecff1f911fbb90ef76e63832710067c72b5d
Arpan Kumar <arpan.kumar@microchip.com>
2021-01-29 14:39:45 +0530

    [SERCOM] Regenerated demo for sercom_usart_usart_echo_blocking_sam_da1_xpro
    MH3-48914
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/sercom/usart/usart_echo_blocking/firmware/src/config/sam_da1_xpro/initialization.c
+++ apps/sercom/usart/usart_echo_blocking/firmware/src/config/sam_da1_xpro/initialization.c
@@ -122,6 +122,7 @@
 
 void SYS_Initialize ( void* data )
 {
+
     NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3);
 
   
@@ -135,6 +136,8 @@ void SYS_Initialize ( void* data )
     SERCOM3_USART_Initialize();
 
     NVMCTRL_Initialize( );
+
+    EVSYS_Initialize();
 
 
 
