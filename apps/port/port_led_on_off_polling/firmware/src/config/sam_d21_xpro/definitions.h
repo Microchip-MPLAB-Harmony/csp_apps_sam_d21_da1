5a992f3fa09cb7a8e7f55f60c4575d8d4811faeb
Arpan Kumar <arpan.kumar@microchip.com>
2021-01-29 14:26:21 +0530

    [PORT] Regenerated demo for port_port_led_on_off_polling_sam_d21_xpro
    MH3-48893
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/port/port_led_on_off_polling/firmware/src/config/sam_d21_xpro/definitions.h
+++ apps/port/port_led_on_off_polling/firmware/src/config/sam_d21_xpro/definitions.h
@@ -49,6 +49,7 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
+#include "peripheral/evsys/plib_evsys.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
@@ -60,6 +61,9 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* CPU clock frequency */
+#define CPU_CLOCK_FREQUENCY 48000000
 
 // *****************************************************************************
 // *****************************************************************************
