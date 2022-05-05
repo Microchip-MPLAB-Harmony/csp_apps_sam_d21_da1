441b09872ea4e56327b3668dc4c38edb3bc03920
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-05 14:25:01 +0530

    [AC] Regenerated demo for ac_ac_sleepwalk_singleshot_sam_da1_xpro
    MH3-67516
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/ac/ac_sleepwalk_singleshot/firmware/src/config/sam_da1_xpro/peripheral/ac/plib_ac.c
+++ apps/ac/ac_sleepwalk_singleshot/firmware/src/config/sam_da1_xpro/peripheral/ac/plib_ac.c
@@ -47,7 +47,7 @@
 #include "plib_ac.h"
 #include "interrupts.h"
 
-AC_OBJECT acObj;
+static AC_OBJECT acObj;
 
 // *****************************************************************************
 // *****************************************************************************
@@ -65,9 +65,9 @@ void AC_Initialize(void)
     }
      
     /**************** Comparator 0 Configurations ************************/ 
-    AC_REGS->AC_COMPCTRL[0] = AC_COMPCTRL_MUXPOS_PIN0 | AC_COMPCTRL_MUXNEG_BANDGAP | AC_COMPCTRL_INTSEL_EOC | AC_COMPCTRL_OUT_OFF | AC_COMPCTRL_SPEED(0) | AC_COMPCTRL_FLEN_OFF | AC_COMPCTRL_SINGLE_Msk;
+    AC_REGS->AC_COMPCTRL[0] = AC_COMPCTRL_MUXPOS_PIN0 | AC_COMPCTRL_MUXNEG_BANDGAP | AC_COMPCTRL_INTSEL_EOC | AC_COMPCTRL_OUT_OFF | AC_COMPCTRL_SPEED(0U) | AC_COMPCTRL_FLEN_OFF | AC_COMPCTRL_SINGLE_Msk;
     AC_REGS->AC_COMPCTRL[0] |= AC_COMPCTRL_ENABLE_Msk;
-    AC_REGS->AC_SCALER[0] = 0;
+    AC_REGS->AC_SCALER[0] = 0U;
 
     AC_REGS->AC_EVCTRL =  AC_EVCTRL_COMPEI0_Msk;
     AC_REGS->AC_INTENSET =  AC_INTENSET_COMP0_Msk;
@@ -82,7 +82,7 @@ void AC_Initialize(void)
 void AC_Start( AC_CHANNEL channel_id )
 {
     /* Start Comparison */
-    AC_REGS->AC_CTRLB |= (1 << channel_id);
+    AC_REGS->AC_CTRLB |= (1U << (uint8_t)channel_id);
 }
 
 void AC_SetVddScalar( AC_CHANNEL channel_id , uint8_t vdd_scalar)
@@ -119,7 +119,7 @@ void AC_ChannelSelect( AC_CHANNEL channel_id , AC_POSINPUT positiveInput, AC_NEG
         /* Wait for Synchronization */
     }
     AC_REGS->AC_COMPCTRL[channel_id] &= ~(AC_COMPCTRL_MUXPOS_Msk | AC_COMPCTRL_MUXNEG_Msk);
-    AC_REGS->AC_COMPCTRL[channel_id] |= (positiveInput | negativeInput);
+    AC_REGS->AC_COMPCTRL[channel_id] |= ((uint32_t)positiveInput | (uint32_t)negativeInput);
 
     /* Enable comparator channel */
     AC_REGS->AC_COMPCTRL[channel_id] |= AC_COMPCTRL_ENABLE_Msk;
@@ -134,9 +134,9 @@ bool AC_StatusGet (AC_CHANNEL channel)
 {
     bool breturnVal = false;
 
-    if((AC_REGS->AC_STATUSB & (AC_STATUSB_READY0_Msk << channel)) == (AC_STATUSB_READY0_Msk << channel))
+    if((AC_REGS->AC_STATUSB & (AC_STATUSB_READY0_Msk << (uint8_t)channel)) == (AC_STATUSB_READY0_Msk << (uint8_t)channel))
     {
-        if((AC_REGS->AC_STATUSA & (AC_STATUSA_STATE0_Msk << channel)) == (AC_STATUSA_STATE0_Msk << channel))
+        if((AC_REGS->AC_STATUSA & (AC_STATUSA_STATE0_Msk << (uint8_t)channel)) == (AC_STATUSA_STATE0_Msk << (uint8_t)channel))
         {
             breturnVal = true;
         }
