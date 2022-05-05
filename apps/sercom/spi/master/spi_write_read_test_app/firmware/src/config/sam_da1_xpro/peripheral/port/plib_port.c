79a1d1f0fb7d8b63466b2f630a00d073f77aa954
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-05 15:11:38 +0530

    [SERCOM] Regenerated demo for sercom_spi_master_spi_write_read_test_app_sam_da1_xpro
    MH3-67566
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/sercom/spi/master/spi_write_read_test_app/firmware/src/config/sam_da1_xpro/peripheral/port/plib_port.c
+++ apps/sercom/spi/master/spi_write_read_test_app/firmware/src/config/sam_da1_xpro/peripheral/port/plib_port.c
@@ -52,7 +52,6 @@
 // Section: PORT Implementation
 // *****************************************************************************
 // *****************************************************************************
-
 // *****************************************************************************
 /* Function:
 
@@ -80,6 +79,7 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PINCFG[18] = 0x1;
    PORT_REGS->GROUP[0].PORT_PINCFG[19] = 0x1;
 
+   PORT_REGS->GROUP[0].PORT_PMUX[7] = 0x0;
    PORT_REGS->GROUP[0].PORT_PMUX[8] = 0x2;
    PORT_REGS->GROUP[0].PORT_PMUX[9] = 0x22;
 
@@ -88,9 +88,10 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[1].PORT_OUT = 0x40000000;
    PORT_REGS->GROUP[1].PORT_PINCFG[30] = 0x0;
 
+   PORT_REGS->GROUP[1].PORT_PMUX[15] = 0x0;
+
 
 }
-
 // *****************************************************************************
 /* Function:
     uint32_t PORT_GroupRead(PORT_GROUP group)
