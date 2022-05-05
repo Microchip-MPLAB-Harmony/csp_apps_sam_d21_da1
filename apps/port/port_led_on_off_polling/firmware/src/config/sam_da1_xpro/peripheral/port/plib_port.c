53cdd2e5b2a628925eddd1d4d649580f327ce791
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-05 14:54:13 +0530

    [PORT] Regenerated demo for port_port_led_on_off_polling_sam_da1_xpro
    MH3-67549
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/port/port_led_on_off_polling/firmware/src/config/sam_da1_xpro/peripheral/port/plib_port.c
+++ apps/port/port_led_on_off_polling/firmware/src/config/sam_da1_xpro/peripheral/port/plib_port.c
@@ -52,7 +52,6 @@
 // Section: PORT Implementation
 // *****************************************************************************
 // *****************************************************************************
-
 // *****************************************************************************
 /* Function:
 
@@ -75,15 +74,17 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_OUT = 0x8000;
    PORT_REGS->GROUP[0].PORT_PINCFG[15] = 0x6;
 
+   PORT_REGS->GROUP[0].PORT_PMUX[7] = 0x0;
 
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x40000000;
    PORT_REGS->GROUP[1].PORT_OUT = 0x40000000;
    PORT_REGS->GROUP[1].PORT_PINCFG[30] = 0x0;
 
+   PORT_REGS->GROUP[1].PORT_PMUX[15] = 0x0;
+
 
 }
-
 // *****************************************************************************
 /* Function:
     uint32_t PORT_GroupRead(PORT_GROUP group)
