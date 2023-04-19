1ffeb2b8d21cb091c52442f6dbdf58eb5800f2b5
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-04-19 12:30:06 +0530

    [AC] Regenerated demo for ac_ac_wav_gen_sam_d21_xpro
    MH3-78962

--- apps/ac/ac_wav_gen/firmware/src/config/sam_d21_xpro/peripheral/ac/plib_ac.c
+++ apps/ac/ac_wav_gen/firmware/src/config/sam_d21_xpro/peripheral/ac/plib_ac.c
@@ -47,7 +47,7 @@
 #include "plib_ac.h"
 #include "interrupts.h"
 
-static AC_OBJECT acObj;
+volatile static AC_OBJECT acObj;
 
 // *****************************************************************************
 // *****************************************************************************
@@ -63,19 +63,20 @@ void AC_Initialize(void)
     {
         /* Wait for Synchronization */
     }
-     
-    /**************** Comparator 0 Configurations ************************/ 
+
+    /**************** Comparator 0 Configurations ************************/
     AC_REGS->AC_COMPCTRL[0] = AC_COMPCTRL_MUXPOS_PIN0 | AC_COMPCTRL_MUXNEG_VSCALE | AC_COMPCTRL_INTSEL_TOGGLE | AC_COMPCTRL_OUT_ASYNC | AC_COMPCTRL_SPEED(0U) | AC_COMPCTRL_FLEN_OFF ;
     AC_REGS->AC_COMPCTRL[0] |= AC_COMPCTRL_HYST_Msk;
     AC_REGS->AC_COMPCTRL[0] |= AC_COMPCTRL_ENABLE_Msk;
     AC_REGS->AC_SCALER[0] = 16U;
 
     AC_REGS->AC_INTENSET =  AC_INTENSET_COMP0_Msk;
+
+    AC_REGS->AC_CTRLA |= AC_CTRLA_ENABLE_Msk;
     while((AC_REGS->AC_STATUSB & AC_STATUSB_SYNCBUSY_Msk) == AC_STATUSB_SYNCBUSY_Msk)
     {
         /* Wait for Synchronization */
-    }
-    AC_REGS->AC_CTRLA |= AC_CTRLA_ENABLE_Msk;
+    }    
 }
 
 void AC_Start( AC_CHANNEL channel_id )
@@ -91,11 +92,6 @@ void AC_SetVddScalar( AC_CHANNEL channel_id , uint8_t vdd_scalar)
 
 void AC_SwapInputs( AC_CHANNEL channel_id )
 {
-    /* Check Synchronization */
-    while((AC_REGS->AC_STATUSB & AC_STATUSB_SYNCBUSY_Msk) == AC_STATUSB_SYNCBUSY_Msk)
-    {
-        /* Wait for Synchronization */
-    }
     /* Disable comparator before swapping */
     AC_REGS->AC_COMPCTRL[channel_id] &= ~AC_COMPCTRL_ENABLE_Msk;
     /* Check Synchronization to ensure that the comparator is disabled */
@@ -106,6 +102,10 @@ void AC_SwapInputs( AC_CHANNEL channel_id )
     /* Swap inputs of the given comparator */
     AC_REGS->AC_COMPCTRL[channel_id] = AC_COMPCTRL_SWAP_Msk;
     AC_REGS->AC_COMPCTRL[channel_id] |= AC_COMPCTRL_ENABLE_Msk;
+    while((AC_REGS->AC_STATUSB & AC_STATUSB_SYNCBUSY_Msk) == AC_STATUSB_SYNCBUSY_Msk)
+    {
+        /* Wait for Synchronization */
+    }    
 }
 
 void AC_ChannelSelect( AC_CHANNEL channel_id , AC_POSINPUT positiveInput, AC_NEGINPUT negativeInput)
@@ -125,7 +125,7 @@ void AC_ChannelSelect( AC_CHANNEL channel_id , AC_POSINPUT positiveInput, AC_NEG
     while((AC_REGS->AC_STATUSB & AC_STATUSB_SYNCBUSY_Msk) == AC_STATUSB_SYNCBUSY_Msk)
     {
         /* Wait for Synchronization */
-    } 
+    }
 
 }
 
@@ -154,16 +154,21 @@ void AC_CallbackRegister (AC_CALLBACK callback, uintptr_t context)
     acObj.context = context;
 }
 
-void AC_InterruptHandler( void )
+void __attribute__((used)) AC_InterruptHandler( void )
 {
+    /* Additional local variable to prevent MISRA C violations (Rule 13.x) */
+    uintptr_t context;
+    uint8_t status;
+    context = acObj.context;      
     /* Copy the status to use inside the callback */
     acObj.int_flags = AC_REGS->AC_STATUSA;
+    status = acObj.int_flags;
     /* Clear the interrupt flags*/
     AC_REGS->AC_INTFLAG = AC_INTFLAG_Msk;
 
     /* Callback user function */
     if(acObj.callback != NULL)
     {
-        acObj.callback(acObj.int_flags, acObj.context);
+        acObj.callback(status, context);
     }
 }
--- apps/ac/ac_wav_gen/firmware/src/main.c
+++ apps/ac/ac_wav_gen/firmware/src/main.c
@@ -81,7 +81,7 @@ void TC3_CallBack_Function (TC_TIMER_STATUS status, uintptr_t context)
         }
 }
 
-uint8_t vdd_scalar   = VDDSCALAR_REF_LOW_LIMIT;
+static uint8_t vdd_scalar   = VDDSCALAR_REF_LOW_LIMIT;
 
 void switch_handler(uintptr_t context )
 {
