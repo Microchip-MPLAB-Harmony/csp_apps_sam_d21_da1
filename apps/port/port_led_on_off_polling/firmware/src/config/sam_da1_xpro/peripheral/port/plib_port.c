7f79f81685f561c5ba09056d8006fdb3f838f136
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-04-19 12:37:08 +0530

    [PORT] Regenerated demo for port_port_led_on_off_polling_sam_da1_xpro
    MH3-78994

--- apps/port/port_led_on_off_polling/firmware/src/config/sam_da1_xpro/peripheral/port/plib_port.c
+++ apps/port/port_led_on_off_polling/firmware/src/config/sam_da1_xpro/peripheral/port/plib_port.c
@@ -71,17 +71,17 @@
 void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
-   PORT_REGS->GROUP[0].PORT_OUT = 0x8000;
-   PORT_REGS->GROUP[0].PORT_PINCFG[15] = 0x6;
+   PORT_REGS->GROUP[0].PORT_OUT = 0x8000U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[15] = 0x6U;
 
-   PORT_REGS->GROUP[0].PORT_PMUX[7] = 0x0;
+   PORT_REGS->GROUP[0].PORT_PMUX[7] = 0x0U;
 
    /************************** GROUP 1 Initialization *************************/
-   PORT_REGS->GROUP[1].PORT_DIR = 0x40000000;
-   PORT_REGS->GROUP[1].PORT_OUT = 0x40000000;
-   PORT_REGS->GROUP[1].PORT_PINCFG[30] = 0x0;
+   PORT_REGS->GROUP[1].PORT_DIR = 0x40000000U;
+   PORT_REGS->GROUP[1].PORT_OUT = 0x40000000U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[30] = 0x0U;
 
-   PORT_REGS->GROUP[1].PORT_PMUX[15] = 0x0;
+   PORT_REGS->GROUP[1].PORT_PMUX[15] = 0x0U;
 
 
 }
