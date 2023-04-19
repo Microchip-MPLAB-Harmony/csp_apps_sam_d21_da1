146a675f86498ff8afe9b36857f342f5586239fa
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-04-19 12:37:16 +0530

    [SERCOM] Regenerated demo for sercom_spi_slave_spi_write_read_sam_da1_xpro
    MH3-79159

--- apps/sercom/spi/slave/spi_write_read/firmware/src/config/sam_da1_xpro/peripheral/port/plib_port.c
+++ apps/sercom/spi/slave/spi_write_read/firmware/src/config/sam_da1_xpro/peripheral/port/plib_port.c
@@ -71,13 +71,13 @@
 void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
-   PORT_REGS->GROUP[0].PORT_PINCFG[16] = 0x1;
-   PORT_REGS->GROUP[0].PORT_PINCFG[17] = 0x1;
-   PORT_REGS->GROUP[0].PORT_PINCFG[18] = 0x1;
-   PORT_REGS->GROUP[0].PORT_PINCFG[19] = 0x1;
+   PORT_REGS->GROUP[0].PORT_PINCFG[16] = 0x1U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[17] = 0x1U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[18] = 0x1U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[19] = 0x1U;
 
-   PORT_REGS->GROUP[0].PORT_PMUX[8] = 0x22;
-   PORT_REGS->GROUP[0].PORT_PMUX[9] = 0x22;
+   PORT_REGS->GROUP[0].PORT_PMUX[8] = 0x22U;
+   PORT_REGS->GROUP[0].PORT_PMUX[9] = 0x22U;
 
    /************************** GROUP 1 Initialization *************************/
 
