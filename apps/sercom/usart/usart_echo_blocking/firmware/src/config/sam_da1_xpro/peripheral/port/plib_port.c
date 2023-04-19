6dc34e469932695fff1cad4ab11baee5d704490a
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-04-19 12:37:17 +0530

    [SERCOM] Regenerated demo for sercom_usart_usart_echo_blocking_sam_da1_xpro
    MH3-79161

--- apps/sercom/usart/usart_echo_blocking/firmware/src/config/sam_da1_xpro/peripheral/port/plib_port.c
+++ apps/sercom/usart/usart_echo_blocking/firmware/src/config/sam_da1_xpro/peripheral/port/plib_port.c
@@ -71,16 +71,16 @@
 void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
-   PORT_REGS->GROUP[0].PORT_PINCFG[22] = 0x1;
-   PORT_REGS->GROUP[0].PORT_PINCFG[23] = 0x1;
+   PORT_REGS->GROUP[0].PORT_PINCFG[22] = 0x1U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[23] = 0x1U;
 
-   PORT_REGS->GROUP[0].PORT_PMUX[11] = 0x22;
+   PORT_REGS->GROUP[0].PORT_PMUX[11] = 0x22U;
 
    /************************** GROUP 1 Initialization *************************/
-   PORT_REGS->GROUP[1].PORT_DIR = 0x40000000;
-   PORT_REGS->GROUP[1].PORT_PINCFG[30] = 0x0;
+   PORT_REGS->GROUP[1].PORT_DIR = 0x40000000U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[30] = 0x0U;
 
-   PORT_REGS->GROUP[1].PORT_PMUX[15] = 0x0;
+   PORT_REGS->GROUP[1].PORT_PMUX[15] = 0x0U;
 
 
 }
