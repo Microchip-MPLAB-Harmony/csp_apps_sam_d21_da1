43ebed610a67e78a122f3deccca59b1a0b10dd7e
Pranjal Das <pranjal.das@microchip.com>
2024-10-21 14:36:05 +0530

    [RTC] Regenerated demo for rtc__rtc_periodic_timeout__firmware__sam_d21_xpro.X
    MH3-101690

--- apps/rtc/rtc_periodic_timeout/firmware/src/config/sam_d21_xpro/definitions.h
+++ apps/rtc/rtc_periodic_timeout/firmware/src/config/sam_d21_xpro/definitions.h
@@ -64,13 +64,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAMD21J18A"
-#define DEVICE_ARCH			 "CORTEX-M0PLUS"
-#define DEVICE_FAMILY		 "SAMD"
-#define DEVICE_SERIES		 "SAMD21"
+#define DEVICE_NAME          "ATSAMD21J18A"
+#define DEVICE_ARCH          "CORTEX-M0PLUS"
+#define DEVICE_FAMILY        "SAMD"
+#define DEVICE_SERIES        "SAMD21"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 47972352
+#define CPU_CLOCK_FREQUENCY 47972352U
 
 // *****************************************************************************
 // *****************************************************************************
