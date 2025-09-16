8b9f7bc1bec26e9a73568f18bdab80bf855f42a0
Syam Nair <Syam.Nair@microchip.com>
2025-09-16 15:16:35 +0000

    [APP] Regenerated demo for ac__ac_sleepwalk_singleshot__firmware
    MH3C-289

--- apps/ac/ac_sleepwalk_singleshot/firmware/src/config/sam_da1_xpro/definitions.h
+++ apps/ac/ac_sleepwalk_singleshot/firmware/src/config/sam_da1_xpro/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -56,8 +56,8 @@
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/pm/plib_pm.h"
-#include "peripheral/rtc/plib_rtc.h"
 #include "peripheral/ac/plib_ac.h"
+#include "peripheral/rtc/plib_rtc.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
--- apps/ac/ac_sleepwalk_singleshot/firmware/src/config/sam_da1_xpro/initialization.c
+++ apps/ac/ac_sleepwalk_singleshot/firmware/src/config/sam_da1_xpro/initialization.c
@@ -15,7 +15,7 @@
 
 // DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -173,9 +173,9 @@ void SYS_Initialize ( void* data )
 
     EVSYS_Initialize();
 
+    AC_Initialize();
     RTC_Initialize();
 
-    AC_Initialize();
 
     NVIC_Initialize();
 
