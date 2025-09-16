7d353e184aa6ca4bb4fa738b67a036b97a76b71e
Syam Nair <Syam.Nair@microchip.com>
2025-09-16 15:10:44 +0000

    [APP] Regenerated demo for adc__adc_dmac_sleepwalking__firmware
    MH3C-289

--- apps/adc/adc_dmac_sleepwalking/firmware/src/config/sam_d21_xpro/peripheral/dmac/plib_dmac.c
+++ apps/adc/adc_dmac_sleepwalking/firmware/src/config/sam_d21_xpro/peripheral/dmac/plib_dmac.c
@@ -76,7 +76,7 @@ typedef struct
  static  dmac_descriptor_registers_t  descriptor_section[DMAC_CHANNELS_NUMBER]    __ALIGNED(8);
 
 /* DMAC Channels object information structure */
-volatile static DMAC_CH_OBJECT dmacChannelObj[DMAC_CHANNELS_NUMBER];
+static volatile DMAC_CH_OBJECT dmacChannelObj[DMAC_CHANNELS_NUMBER];
 
 // *****************************************************************************
 // *****************************************************************************
