143da1569f35ae6bda21e4e15fb7dd78731158dc
Syam Nair <Syam.Nair@microchip.com>
2025-09-16 15:13:07 +0000

    [APP] Regenerated demo for sercom__spi__master__spi_self_loopback_interrupt__firmware
    MH3C-289

--- apps/sercom/spi/master/spi_self_loopback_interrupt/firmware/src/config/sam_da1_xpro/peripheral/sercom/spi_master/plib_sercom1_spi_master.c
+++ apps/sercom/spi/master/spi_self_loopback_interrupt/firmware/src/config/sam_da1_xpro/peripheral/sercom/spi_master/plib_sercom1_spi_master.c
@@ -62,7 +62,7 @@
 #define SERCOM1_SPIM_BAUD_VALUE         (23UL)
 
 /*Global object to save SPI Exchange related data  */
-volatile static SPI_OBJECT sercom1SPIObj;
+static volatile SPI_OBJECT sercom1SPIObj;
 
 // *****************************************************************************
 // *****************************************************************************
